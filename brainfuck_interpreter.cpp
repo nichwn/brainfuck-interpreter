08e508be5486b63f80e75183b7ccc9274347935f
Nicholas Brown <nichwn@gmail.com>
2014-01-02 01:34:40 +1100

    The interpreter appears to be functional. A few more minor things remain to be implemented.

---
+++ brainfuck_interpreter.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <deque>
+#include <fstream>
+
+using std::vector;
+using std::stack;
+using std::cout;
+using std::cin;
+using std::ifstream;
+
+/****************************************************************/
+
+/* function prototypes */
+void read(vector<char> &i_vc, ifstream &is);
+void interp(vector<unsigned char> &cells, vector<char> &ivc);
+vector<char>::iterator skip_block(vector<char>::iterator i_it);
+
+/****************************************************************/
+
+/* reads brainfuck code into memory 
+ */
+void read(vector<char> &i_vc, ifstream &is) {
+	char c;
+	is.get(c);
+	while (!is.eof()) {
+		i_vc.push_back(c);
+		is.get(c);
+	}
+}
+
+/****************************************************************/
+
+/* interprets the brainfuck code, and performs the actions
+ */
+void interp(vector<unsigned char> &cells, vector<char> &i_vc) {
+	stack<vector<char>::iterator> block_front;
+	auto c_it = cells.begin();
+	
+	// scan through the code character by character
+	for (auto i_it = i_vc.begin(); i_it != i_vc.end(); ++i_it) {
+		if (*i_it == '>') {
+			++c_it;
+		} else if (*i_it == '<') {
+			--c_it;
+		} else if (*i_it == '+') {
+			++(*c_it);
+		} else if (*i_it == '-') {
+			--(*c_it);
+		} else if (*i_it == '.') {
+			cout.put(*c_it);
+		} else if (*i_it == ',') {
+			*c_it = cin.get();
+		} else if (*i_it == '[') {
+			if (*c_it) {
+				block_front.push(i_it);
+			} else {
+				i_it = skip_block(i_it);
+			}
+		} else if (*i_it == ']') {
+			if (*c_it) {
+				i_it = block_front.top();
+			} else {
+				block_front.pop();
+			}
+		}
+	}
+}
+
+/****************************************************************/
+
+/* skips to the end of a block of brainfuck code, indicated via [ and ], and
+ * returns an iterator to it
+ */
+vector<char>::iterator skip_block(vector<char>::iterator i_it) {
+	int count = 0;
+	for (++i_it; ; ++i_it) {
+		if (*i_it == ']' && count == 0) {
+			return i_it;
+		} else if (*i_it == ']') {
+			--count;
+		} else if (*i_it == '[') {
+			++count;
+		}
+	}
+}
+
+/****************************************************************/
+
+int main() {
+	// TODO - add options for other brainfuck code locations
+	// TODO - improve interface
+	const int Cells = 30000;
+	vector<unsigned char> cells(Cells, 0);
+	vector<char> i_vc;
+	ifstream is("hello_world.b");
+	read(i_vc, is);
+	interp(cells, i_vc);
+	return 0;
+}
+
